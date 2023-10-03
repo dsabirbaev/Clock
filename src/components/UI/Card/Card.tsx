821e0cdc51ae24c824978b246073463c598d7904
Davranbek <davran9559@gmail.com>
2023-10-04 01:47:24 +0500

    card

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,14 +1,8 @@
-import  {FC} from 'react';
 
-interface PROPS {
-    title: string,
-    description: string,
-    img: string, 
-    link: string, 
-    speed:string
-}
 
-const Card: FC<{ case: PROPS }> = ({case:{title , description , img , link , speed}}) => {
+
+
+const Card = () => {
     return (
         <div className='card'>
            <h2>card</h2>
