1112f670472661e0b9d0c0bdbf6dcc7c9bc2e09d
Davranbek <davran9559@gmail.com>
2023-10-04 01:04:27 +0500

    project

---
+++ src/pages/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+
+
+
+import Intro from "../Intro/Intro";
+import About from "../About/About";
+
+const Layout = () => {
+    return (
+        <>
+            <Intro/>
+        </>
+    );
+};
+
+export default Layout;
